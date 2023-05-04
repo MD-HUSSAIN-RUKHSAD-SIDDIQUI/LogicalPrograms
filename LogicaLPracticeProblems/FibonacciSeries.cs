8d2046308c8b36038004ef7d73b2d812c29bac4f
MD HUSSAIN <mdhussainrukhsadsiddiqui@gmail.com>
2023-05-04 20:02:56 +0530

    Welcome to Logical Practice Programs

---
+++ LogicaLPracticeProblems/FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaLPracticeProblems
+{
+    internal class FibonacciSeries
+    {
+        public static void Find()
+        {
+            int n1 = 0, n2 = 1, n, i, totalNum;
+            Console.WriteLine("Enter the Number of Elements to be Displayed in Fibonacci Series :");
+            totalNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");
+            for (i = 2; i < totalNum; i++)
+            {
+                n = n1 + n2;
+                Console.Write(n + " ");
+                n1 = n2;
+                n2 = n;
+            }
+        }
+    }
+}
---
+++ LogicaLPracticeProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaLPracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to logical Programs:");
+            //FibonacciSeries.Find();
+            Console.ReadKey();
+        }
+    }
+}
